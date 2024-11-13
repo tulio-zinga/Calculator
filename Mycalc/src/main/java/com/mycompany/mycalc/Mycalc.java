358ef3f289f2897d10b0b1eda2da858f4f852fbe
Tulio Zynga <felipeseila38@gmail.com>
2024-11-13 08:19:42 -0300

    Projeto Calc

---
+++ Mycalc/src/main/java/com/mycompany/mycalc/Mycalc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mycalc;
+
+/**
+ *
+ * @author 16030076604
+ */
+public class Mycalc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
